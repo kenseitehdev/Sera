cd48063d58ac7955727b1d3efd44aebccea25e3e
Kensei <tehkensei@gmail.com>
2025-10-23 19:53:29 -0500

    restarted Sera server

--- include/response.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESPONSE_H
-#define RESPONSE_H
-#include "sera.h"
-
-void send_http_response(int client_fd, http_response_t *res);
-void free_http_response(http_response_t *res);
-
-#endif
--- include/router.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef ROUTER_H
-#define ROUTER_H
-#include "sera.h"
-
-handler_func_t find_handler(const char *path);
-
-#endif
